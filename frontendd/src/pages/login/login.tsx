1b9c4820a14fdf78f44aaa29a3b1ba5b327f09c1
Caio Lucas <caiolucasprogramador@gmail.com>
2025-05-16 20:17:09 -0300

    Login

--- frontendd/src/pages/login/login.tsx
+++ frontendd/src/pages/login/login.tsx
@@ -3,13 +3,27 @@ const Login:React.FC=()=>{
 
 
     return(
-
-        
-        <div>
-            <p>oi</p>
+<div>
+      <h1>Registro</h1>
+      <form id="user-register">
+        <div id="user-name-container">
+          <label htmlFor="user-name">Usuário:</label>
+          <input type="text" id="user-name" />
         </div>
-    )
-}
-
-
+        <div id="user-password-container">
+          <label htmlFor="user-password">Senha:</label>
+          <input type="password" id="user-password" />
+        </div>
+        <div id="register-actions">
+          <button type="submit">Criar conta</button>
+        </div>
+      </form>
+      <div id="login-link">
+        <p id="text">
+          Já possui conta? <a href="/login">Faça seu login</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default Login
