940cc549737a6043bb15751449f449950bdef745
Ravel Costa <ravelcostadearaujo@gmail.com>
2025-05-28 10:03:02 -0300

    Adicionando a API

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+
+    descricao = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+    def __str__(self):
+        return self.descricao
+
